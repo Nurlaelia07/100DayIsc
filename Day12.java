944d7778b3cdbba25518b8e61e5ba2df8ac51290
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-10-20 21:27:34 +0800

    Perulangan While

---
+++ Day12.java
@@ -0,0 +1,10 @@
+public class ISC
+ {
+   public static void main(String args[])
+   {
+     boolean a = true;  //Menginisialisasi variable a dengan tipe data boolean dengan nilai true
+     while (a == true) { //Memulai perulangan dengan kodisi a sama dengan true, dan tidak akan berhenti sampai nilai a sama dengan false
+      System.out.println("Nurlaelia"); //mencetak teks Nurlaelia
+    }
+   }
+}

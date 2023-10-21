eea0d813b76501b58edd65c7cf3267374e08816f
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-10-21 21:17:40 +0800

    Perulangan do-while

---
+++ Day13.java
@@ -0,0 +1,13 @@
+package ngoding;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        // Perulangan do-while untuk mencetak angka dari 1 sampai 20
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a <= 20);
+    }
+}

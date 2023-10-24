0442f416e4cff7559a8104d8418c21204e3826f4
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-10-24 19:04:15 +0800

    Menampilkan matriks 2 x 2

---
+++ Day16.java
@@ -0,0 +1,16 @@
+public class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    // Matriks 2x2
+        int[][] matrix = {
+            {7, 5},
+            {3, 4}
+        };
+
+        // Menampilkan matriks 
+        System.out.println("Matriks 2x2:");
+        System.out.println(matrix[0][0] + " " + matrix[0][1]);
+        System.out.println(matrix[1][0] + " " + matrix[1][1]);
+    }
+   }

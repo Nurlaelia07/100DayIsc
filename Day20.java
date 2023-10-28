4e7d400121e6257aed76ae3f1759fbf03948f190
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-10-28 17:13:12 +0800

    Perulangan mencetak angka di list secara terbalik

---
+++ Day20.java
@@ -0,0 +1,15 @@
+package ngoding;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+        // List angka
+        int[] angka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        //Perulangan untuk mencetak angka dilist
+        for (int i = angka.length - 1; i >= 0; i--) {
+            System.out.println(angka[i]);
+        }
+    }
+
+}

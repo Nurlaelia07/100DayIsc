74d4feb23a59f1ffa30d2e999f982e4c5415274d
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-11-12 19:27:36 +0800

    Inputan angka Menentukan bil prima

---
+++ Day35.java
@@ -0,0 +1,18 @@
+package ngoding;
+
+import java.util.Scanner;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print ("Masukkan angka : ");
+        int angka = sc.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan prima");
+        } else {
+            System.out.println(angka + " bukan bilangan prima");
+        }
+
+    }
+}

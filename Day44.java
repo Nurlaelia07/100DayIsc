1e167b8e5bd6cd38bd64b01e5a46fc7d7c8c1ef0
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-11-21 15:20:09 +0800

    ArrayList

---
+++ Day44.java
@@ -0,0 +1,24 @@
+package ngoding;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day44 {
+
+    public static void main(String[] args) {
+        ArrayList namaMahasiswa = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Masukkan nama mahasiswa : ");
+            String NmaMhs = sc.nextLine();
+            namaMahasiswa.add(NmaMhs);
+            System.out.println("Nama nama Mahasiswa : " + namaMahasiswa);
+
+        }
+
+        namaMahasiswa.set(1, "Laelia");
+        System.out.println("Nama Mahasiswa setelah di update " + namaMahasiswa);
+
+    }
+}

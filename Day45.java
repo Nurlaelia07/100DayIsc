40ef0762e525b108e2764ab16c8fa381cfe76514
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2023-11-22 19:24:42 +0800

    Array String

---
+++ Day45.java
@@ -0,0 +1,21 @@
+package ngoding;
+
+public class Day45 {
+
+    public static void main(String[] args) {
+        // Deklarasi array
+        String[] nama = new String[5];
+
+        // Mengisi array
+        nama[0] = "Laelia";
+        nama[1] = "Nike";
+        nama[2] = "FItta";
+        nama[3] = "Lala";
+        nama[4] = "AMalia";
+
+        // Menampilkan array
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+    }
+}

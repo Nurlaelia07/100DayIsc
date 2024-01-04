fe331da4765aba3f68e008c6fd9d5e3193bac24e
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2024-01-04 08:09:40 +0800

    Update Day88.java

--- Day88.java
+++ Day88.java
@@ -8,20 +8,16 @@ public class Day88 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         // Buat arraylist untuk menampung peralatan kantor
         ArrayList<String> peralatanKantor = new ArrayList<>();
-
         // Input jumlah peralatan kantor
         System.out.print("Masukkan jumlah peralatan kantor: ");
         int jumlahPeralatanKantor = input.nextInt();
-
         // Input nama peralatan kantor
         for (int i = 0; i < jumlahPeralatanKantor; i++) {
             System.out.print("Masukkan nama peralatan kantor ke-" + (i + 1) + ": ");
             peralatanKantor.add(input.next());
         }
-
         // Tampilkan peralatan kantor
         System.out.println("Peralatan kantor:");
         for (String peralatan : peralatanKantor) {

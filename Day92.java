2322b0767eb57f790e61b61beb3d6ebfeef290d6
Nurlaelia07 <100515785+Nurlaelia07@users.noreply.github.com>
2024-01-08 08:32:40 +0800

    Day92 StringBuffer

---
+++ Day92.java
@@ -0,0 +1,23 @@
+package ngoding;
+public class Day92 {
+    public static void main(String[] args) {
+        // Membuat objek StringBuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        // Menambahkan nama lengkap
+        stringBuffer.append("Nama lengkap: ");
+        stringBuffer.append("Nurlaelia");
+        // Menambahkan NIM
+        stringBuffer.append(", NIM: ");
+        stringBuffer.append("0221099");
+        // Menambahkan kelas
+        stringBuffer.append(", Kelas: ");
+        stringBuffer.append("Informatika G");
+        // Menambahkan universitas
+        stringBuffer.append(", Universitas: ");
+        stringBuffer.append("Universitas Sulawesi Barat");
+        // Mencetak stringBuffer
+        System.out.println(stringBuffer);
+    }
+}
+
+  
